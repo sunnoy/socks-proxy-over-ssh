6f35698cf501c17b52651206ebc4794999c12514
lirui <ttzhsz@163.com>
2022-11-26 21:30:54 +0800

    add socks5 user and password

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module socks-proxy-over-ssh
 
 go 1.19
+
+require github.com/things-go/go-socks5 v0.0.2 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"github.com/things-go/go-socks5"
+	"log"
+	"os"
+	"sync"
+)
 
 var (
 	sshHost     = ""
@@ -8,7 +13,9 @@ var (
 	sshUser     = "root"
 	sshPassword = ""
 
-	socks5Listen = "localhost:1989"
+	socks5Listen   = "localhost:1989"
+	socks5User     = ""
+	socks5Password = ""
 
 	sshListen = "0.0.0.0:3000"
 )
@@ -16,6 +23,30 @@ var (
 func main() {
 	getEnv()
 
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	// do ray sock5 proxy
+	go func() {
+		credentials := socks5.StaticCredentials{
+			socks5User: socks5Password,
+		}
+
+		cator := socks5.UserPassAuthenticator{Credentials: credentials}
+
+		// Create a SOCKS5 server
+		server := socks5.NewServer(
+			socks5.WithAuthMethods([]socks5.Authenticator{cator}),
+		)
+
+		log.Println("开始启动服务端！")
+
+		if err := server.ListenAndServe("tcp", ":1989"); err != nil {
+			panic(err)
+		}
+
+		wg.Done()
+	}()
 }
 
 // get args from env
@@ -25,5 +56,8 @@ func getEnv() {
 	sshPort = os.Getenv("SSH_PORT")
 	sshPassword = os.Getenv("SSH_PW")
 	socks5Listen = os.Getenv("SOCKS_L")
+	socks5User = os.Getenv("SOCKSU")
+	socks5Password = os.Getenv("SOCKSPW")
 	sshListen = os.Getenv("SSH_L")
+
 }
